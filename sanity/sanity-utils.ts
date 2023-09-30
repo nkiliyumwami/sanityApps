e33f0bca9427f9e98971754413223fef337207f6
Emmanuel Nkiriyumwami <nkiliyumwami@gmail.com>
2023-09-29 22:59:45 -0400

    fetch projects and single project

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -8,7 +8,7 @@ export async function getProjects(): Promise<Project[]> {
     projectId: "xnosmchu",
     dataset: "production",
     apiVersion: "2023-09-28",
-    useCdn: false,
+    useCdn: true,
   });
   //fetch all projects: using groq query
   return client.fetch(groq`*[ _type == "project"]{
@@ -20,4 +20,26 @@ export async function getProjects(): Promise<Project[]> {
     url,
     content,
      }`);
+}
+
+export async function getProject(slug: string) : Promise<Project> {
+  const client = createClient({
+    projectId: "xnosmchu",
+    dataset: "production",
+    apiVersion: "2023-09-28",
+    useCdn: true,
+  });
+  //fetch single project: using groq query
+  return client.fetch(groq`*[ _type == "project" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    name,
+    'slug': slug.current,
+    'image': image.asset ->url,
+    url,
+    content,
+     }`,
+     //passing the slug 
+     {slug}
+     );
 }
